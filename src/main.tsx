9359670d19a14c8cd32e819e154fc4c47a24e259
CaeHill <hillcaetano@gmail.com>
2026-04-10 15:56:18 -0300

    refactor: move Chakra basis compenents for a dedicate folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from './components/ui/provider';
+import { Provider } from './components/ui/chakraBasis/provider';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
